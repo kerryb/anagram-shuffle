ca80b7ab283a3de0f5e6b27ea469096a331022f2
Kerry Buckley <kerryjbuckley@gmail.com>
2013-01-13 20:02:02 +0000

    Extract DeleteButtonListener as a standalone class

--- src/org/kerryb/android/anagramshuffle/AnagramCursorAdapter.java
+++ src/org/kerryb/android/anagramshuffle/AnagramCursorAdapter.java
@@ -2,11 +2,9 @@ package org.kerryb.android.anagramshuffle;
 
 import org.kerryb.android.anagramshuffle.Database.AnagramsTable;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.database.Cursor;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.SimpleCursorAdapter;
 
 public class AnagramCursorAdapter extends SimpleCursorAdapter {
@@ -22,28 +20,6 @@ public class AnagramCursorAdapter extends SimpleCursorAdapter {
 				.getColumnIndex(AnagramsTable._ID));
 
 		View button = view.findViewById(R.id.deleteButton);
-		button.setOnClickListener(new DeleteButtonListener(id, context));
-	}
-
-	private final class DeleteButtonListener implements OnClickListener {
-		private final String id;
-		private final Context context;
-
-		private DeleteButtonListener(String id, Context context) {
-			this.id = id;
-			this.context = context;
-		}
-
-		public void onClick(View arg0) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(context);
-			builder.setMessage("Are you sure you want to delete this anagram?")
-					.setPositiveButton(
-							"Delete",
-							((MainActivity) context).new ConfirmDeleteListener(
-									id)).setNegativeButton("Cancel", null);
-			;
-			builder.show();
-
-		}
+		button.setOnClickListener(new DeleteButtonListener(context, id));
 	}
 }
---
+++ src/org/kerryb/android/anagramshuffle/DeleteButtonListener.java
@@ -0,0 +1,28 @@
+package org.kerryb.android.anagramshuffle;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+final class DeleteButtonListener implements OnClickListener {
+	private final String anagramId;
+	private final Context context;
+
+	DeleteButtonListener(Context context, String anagramId) {
+		this.context = context;
+		this.anagramId = anagramId;
+	}
+
+	public void onClick(View view) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setMessage("Are you sure you want to delete this anagram?")
+				.setPositiveButton(
+						"Delete",
+						((MainActivity) context).new ConfirmDeleteListener(
+								anagramId)).setNegativeButton("Cancel", null);
+		;
+		builder.show();
+
+	}
+}
